40391966bfb961361d6ad352144f484031eb264d
vincilee <vincilee1994@outlook.com>
2022-08-15 10:16:21 +0800

    dijstra

--- Graph Algorithm/Dijstra's algorithm/solution.py
+++ Graph Algorithm/Dijstra's algorithm/solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+import heapq
+from math import inf
 import sys
 
 
@@ -9,8 +12,23 @@ def inlt():
     return (list(map(int, input().split())))
 
 # construct dag from input data
+m = inp()
 n = inp()
-dag = [{} for _ in range(n)]
-for _ in range(n - 1):
+dag = defaultdict(list)
+for _ in range(n):
     a, b, w = inlt()
-    dag[a].append((b - 1))
+    dag[a].append((b,w))
+print(dag)
+dist = [inf for _ in range(m)]
+dist[0] = 0
+pq = [(0,0)]
+
+while pq:
+    d, u = heapq.heappop(pq)
+    if dist[u] < d:
+        continue
+    for v, w in dag[u]:
+        if dist[v] > dist[u] + w:
+            dist[v] = dist[u] + w
+            heapq.heappush(pq, (dist[v], v))
+print(dist)
